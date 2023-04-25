4353bbd52e787b793c899691118731ac3aadebda
Qihang Wu <31423063+hahacharlie@users.noreply.github.com>
2023-04-25 15:37:50 -0400

    Start reading from the Gryoscope

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,25 @@
 
 #include <mbed.h>
 
-int main()
-{
-  DigitalOut led(LED1);
+volatile float read_value;
+volatile float voltage;
 
-  while (1)
-  {
-    led = !led;
-    thread_sleep_for(500);
+AnalogIn ain(PA_6);
+Timeout t_out;
+volatile bool sample_ready=false;
+void cb(){
+  sample_ready=true;
+}
+int main(){
+  t_out.attach(cb,500ms);
+  while(1){
+    while(!sample_ready){
+      
+    }
+    sample_ready=false;
+    t_out.attach(cb,500ms);
+    read_value=ain.read();
+    voltage=read_value*3.3f;
+    printf("read value: %f  read voltage: %f V\n",read_value,voltage);
   }
 }
